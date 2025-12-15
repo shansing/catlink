0929db88dc3d8cd11058fe8c96eded19f11d296d
snyh <snyh@snyh.org>
2025-12-15 13:52:48 +0800

    dnd: force update pointer position before send request

--- xpra/client/gtk3/window/dragndrop.py
+++ xpra/client/gtk3/window/dragndrop.py
@@ -132,15 +132,19 @@ class DragNDropWindow(GtkStubWindow):
         RIM_SERVER=os.getenv("RIM_SERVER","")
         if RIM_SERVER != "":
             target_xid = self._metadata.get("xid")
+
+            # force update pointer position
+            self._file_handler.poll_pointer()
+
             import requests
-            x = requests.get(
+            resp = requests.get(
                 url =f"{RIM_SERVER}/dnd/drop",
                 params = {
                     "file_path": filelist,
                     "client_id": os.getenv("LZC_CLIENT_ID"),
                     "win": target_xid,
                 })
-            log.info(f"DND DROP RESULT {x} win:{target_xid}")
+            log.info(f"DND DROP RESULT {resp} win:{target_xid}")
         else:
             for filename in filelist:
                 self.drag_process_file(filename, file_done)
