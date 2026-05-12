fd68b21447ae5c752130cf520b4c5117bbab474e
shansing <i@shansing.com>
2026-05-12 16:56:44 +0800

    fix: clipboard clientfs path for lzc multi-instance

--- xpra/gtk/clipboard.py
+++ xpra/gtk/clipboard.py
@@ -175,7 +175,13 @@ class GTKClipboardProxy(ClipboardProxyCore, GObject.GObject):
             return
         if target == "text/uri-list":
             import os
-            prefix = os.path.join("/lzcapp/clientfs/", os.getenv("LZC_CLIENT_ID"))
+            client_id = os.getenv("LZC_CLIENT_ID")
+            uid = os.getenv("LZC_CDE_UID")
+            if uid:
+                prefix = os.path.join("/lzcapp/clientfs/", uid, ".byid", client_id or "")
+            else:
+                log.warn("Warning: LZC_CDE_UID is not set, using legacy clientfs clipboard path but may not work")
+                prefix = os.path.join("/lzcapp/clientfs/", client_id)
             processed_uris = []
             uris = self.clipboard.wait_for_uris()
             if len(uris) == 0:
